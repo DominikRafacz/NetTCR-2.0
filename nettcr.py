caf13dfdfd69222d3570017864f2e683a87c5bcc
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2022-01-18 16:19:40 +0100

    some pep8 fixes

--- nettcr.py
+++ nettcr.py
@@ -1,33 +1,24 @@
-import os, sys
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-import tensorflow as tf
-
+import os
+import sys
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-import keras
-import time
-from keras.models import Sequential, Model
-from keras.layers import Input, Dense, Conv1D, GlobalMaxPooling1D, concatenate
 from tensorflow.keras.optimizers import Adam
-from keras.initializers import glorot_normal
-from keras.activations import sigmoid
-from sklearn.metrics import roc_auc_score
 import utils
-import keras.backend as K
 from keras.callbacks import EarlyStopping
+from nettcr_architectures import nettcr_ab, nettcr_one_chain
+from argparse import ArgumentParser
 
-from nettcr_architectures import nettcr_ab, nettcr_one_chain 
 
-#Options for Pandas DataFrame printing
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# Options for Pandas DataFrame printing
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
-#pd.set_option('display.max_colwidth', -1)
+# pd.set_option('display.max_colwidth', -1)
 
-from argparse import ArgumentParser
 
-#Args parser 
+# Args parser
 parser = ArgumentParser(description="Specifying Input Parameters")
 parser.add_argument("-tr", "--trainfile", help="Specify the full path of the training file with TCR sequences")
 parser.add_argument("-te", "--testfile", help="Specify the full path of the file with TCR sequences")
@@ -38,22 +29,18 @@ args = parser.parse_args()
 
 EPOCHS = int(args.epochs)
 chain = args.chain
-if chain!=["a","b","ab"]:
-    print("Invalid chain. You can select a (alpha), b (beta), ab (alpha+beta)")
 
 print('Loading and encoding the data..')
 train_data = pd.read_csv(args.trainfile)
 test_data = pd.read_csv(args.testfile)
 
-
 # Encode data
 encoding = utils.blosum50_20aa
-early_stop = EarlyStopping(monitor='loss',min_delta=0,
-               patience=10, verbose=0,mode='min',restore_best_weights=True)
-
+early_stop = EarlyStopping(monitor='loss', min_delta=0,
+                           patience=10, verbose=0, mode='min', restore_best_weights=True)
 
 # Call and compile the model
-if chain=='ab':
+if chain == 'ab':
     pep_train = utils.enc_list_bl_max_len(train_data.peptide, encoding, 9)
     tcra_train = utils.enc_list_bl_max_len(train_data.CDR3a, encoding, 30)
     tcrb_train = utils.enc_list_bl_max_len(train_data.CDR3b, encoding, 30)
@@ -66,7 +53,7 @@ if chain=='ab':
     test_inputs = [tcra_test, tcrb_test, pep_test]
 
     mdl = nettcr_ab()
-elif chain=="a":
+elif chain == "a":
     pep_train = utils.enc_list_bl_max_len(train_data.peptide, encoding, 9)
     tcra_train = utils.enc_list_bl_max_len(train_data.CDR3a, encoding, 30)
     y_train = np.array(train_data.binder)
@@ -76,7 +63,7 @@ elif chain=="a":
     train_inputs = [tcra_train, pep_train]
     test_inputs = [tcra_test, pep_test]
     mdl = nettcr_one_chain()
-elif chain=="b":
+elif chain == "b":
     pep_train = utils.enc_list_bl_max_len(train_data.peptide, encoding, 9)
     tcrb_train = utils.enc_list_bl_max_len(train_data.CDR3b, encoding, 30)
     y_train = np.array(train_data.binder)
@@ -86,12 +73,13 @@ elif chain=="b":
     train_inputs = [tcrb_train, pep_train]
     test_inputs = [tcrb_test, pep_test]
     mdl = nettcr_one_chain()
-
+else:
+    raise Exception("Invalid chain. You can select a (alpha), b (beta), ab (alpha+beta)")
 
 mdl.compile(loss="binary_crossentropy", optimizer=Adam(learning_rate=0.001))
 print('Training..')
 # Train
-history = mdl.fit(train_inputs, y_train, 
+history = mdl.fit(train_inputs, y_train,
                   epochs=EPOCHS, batch_size=128, verbose=1, callbacks=[early_stop])
 
 print('Evaluating..')
--- nettcr_architectures.py
+++ nettcr_architectures.py
@@ -1,20 +1,17 @@
-import keras
-from keras.models import Sequential, Model
-from keras.layers import Input, Dense, Conv1D, Flatten
 from keras.layers import GlobalMaxPooling1D
-from keras.layers import concatenate, Dropout, Activation
-from keras.initializers import glorot_normal
-from keras.activations import sigmoid
+from keras.layers import Input, Dense, Conv1D
+from keras.layers import concatenate
+from keras.models import Model
 
 
 def nettcr_ab():
-    '''NetTCR ab with the "correct" pooling dimension, that is: the three towers are convoluted, then pooled
-       and then concatenated for the FNN'''
-    
-    pep_in = Input(shape=(9,20))
-    cdra_in = Input(shape=(30,20))
-    cdrb_in = Input(shape=(30,20))
-       
+    """NetTCR ab with the "correct" pooling dimension, that is: the three towers are convoluted, then pooled
+       and then concatenated for the FNN"""
+
+    pep_in = Input(shape=(9, 20))
+    cdra_in = Input(shape=(30, 20))
+    cdrb_in = Input(shape=(30, 20))
+
     pep_conv1 = Conv1D(16, 1, padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(pep_in)
     pep_pool1 = GlobalMaxPooling1D()(pep_conv1)
     pep_conv3 = Conv1D(16, 3, padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(pep_in)
@@ -25,7 +22,7 @@ def nettcr_ab():
     pep_pool7 = GlobalMaxPooling1D()(pep_conv7)
     pep_conv9 = Conv1D(16, 9, padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(pep_in)
     pep_pool9 = GlobalMaxPooling1D()(pep_conv9)
-    
+
     cdra_conv1 = Conv1D(16, 1, padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(cdra_in)
     cdra_pool1 = GlobalMaxPooling1D()(cdra_conv1)
     cdra_conv3 = Conv1D(16, 3, padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(cdra_in)
@@ -36,7 +33,7 @@ def nettcr_ab():
     cdra_pool7 = GlobalMaxPooling1D()(cdra_conv7)
     cdra_conv9 = Conv1D(16, 9, padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(cdra_in)
     cdra_pool9 = GlobalMaxPooling1D()(cdra_conv9)
-    
+
     cdrb_conv1 = Conv1D(16, 1, padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(cdrb_in)
     cdrb_pool1 = GlobalMaxPooling1D()(cdrb_conv1)
     cdrb_conv3 = Conv1D(16, 3, padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(cdrb_in)
@@ -47,29 +44,28 @@ def nettcr_ab():
     cdrb_pool7 = GlobalMaxPooling1D()(cdrb_conv7)
     cdrb_conv9 = Conv1D(16, 9, padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(cdrb_in)
     cdrb_pool9 = GlobalMaxPooling1D()(cdrb_conv9)
-    
 
     pep_cat = concatenate([pep_pool1, pep_pool3, pep_pool5, pep_pool7, pep_pool9])
     cdra_cat = concatenate([cdra_pool1, cdra_pool3, cdra_pool5, cdra_pool7, cdra_pool9])
     cdrb_cat = concatenate([cdrb_pool1, cdrb_pool3, cdrb_pool5, cdrb_pool7, cdrb_pool9])
 
-    
     cat = concatenate([pep_cat, cdra_cat, cdrb_cat], axis=1)
-    
+
     dense = Dense(32, activation='sigmoid')(cat)
-    
-    #drop = Dropout(0.3)(dense)
-    
+
+    # drop = Dropout(0.3)(dense)
+
     out = Dense(1, activation='sigmoid')(dense)
-    
-    model = (Model(inputs=[cdra_in, cdrb_in, pep_in],outputs=[out]))
-    
+
+    model = (Model(inputs=[cdra_in, cdrb_in, pep_in], outputs=[out]))
+
     return model
 
+
 def nettcr_one_chain():
-    cdr_in = Input(shape=(30,20))
-    pep_in = Input(shape=(9,20))
-    
+    cdr_in = Input(shape=(30, 20))
+    pep_in = Input(shape=(9, 20))
+
     pep_conv1 = Conv1D(16, 1, padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(pep_in)
     pep_pool1 = GlobalMaxPooling1D()(pep_conv1)
     pep_conv3 = Conv1D(16, 3, padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(pep_in)
@@ -80,7 +76,7 @@ def nettcr_one_chain():
     pep_pool7 = GlobalMaxPooling1D()(pep_conv7)
     pep_conv9 = Conv1D(16, 9, padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(pep_in)
     pep_pool9 = GlobalMaxPooling1D()(pep_conv9)
-    
+
     cdr_conv1 = Conv1D(16, 1, padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(cdr_in)
     cdr_pool1 = GlobalMaxPooling1D()(cdr_conv1)
     cdr_conv3 = Conv1D(16, 3, padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(cdr_in)
@@ -91,18 +87,18 @@ def nettcr_one_chain():
     cdr_pool7 = GlobalMaxPooling1D()(cdr_conv7)
     cdr_conv9 = Conv1D(16, 9, padding='same', activation='sigmoid', kernel_initializer='glorot_normal')(cdr_in)
     cdr_pool9 = GlobalMaxPooling1D()(cdr_conv9)
-    
+
     pep_cat = concatenate([pep_pool1, pep_pool3, pep_pool5, pep_pool7, pep_pool9])
     cdr_cat = concatenate([cdr_pool1, cdr_pool3, cdr_pool5, cdr_pool7, cdr_pool9])
-    
+
     cat = concatenate([pep_cat, cdr_cat], axis=1)
-    
+
     dense = Dense(32, activation='sigmoid')(cat)
-    
-    #drop = Dropout(0.3)(dense)
-    
+
+    # drop = Dropout(0.3)(dense)
+
     out = Dense(1, activation='sigmoid')(dense)
-    
-    model = (Model(inputs=[cdr_in, pep_in],outputs=[out]))
-    
+
+    model = (Model(inputs=[cdr_in, pep_in], outputs=[out]))
+
     return model
